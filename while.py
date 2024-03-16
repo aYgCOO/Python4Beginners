1dee02c8023b421df6d5470edb2e562ad9d0407a
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-03-16 15:53:37 +0530

    Update

--- while.py
+++
@@ -1,6 +0,0 @@
-#While loops in python
-'''While loops is same as for loop but the difference is that we need to initilize the loop control variable manually'''
-i = 0
-while(i < 11):
-    print(i)
-    i += 1
